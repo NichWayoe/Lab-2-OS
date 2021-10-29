8e918c672fb326aed90d58f05f5deb72a049fe36
Nicholas Wayoe <nicholas.wayoe@bison.howard.edu>
2021-10-29 15:57:35 +0000

    added timer

--- timer.c
+++ timer.c
@@ -5,10 +5,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <signal.h>
+#include <time.h>
 #include <unistd.h>
 
 int flag = 0;
-int seconds = 0;
+time_t seconds;
+int alarms = 0;
 
 void alarmSignalHandler(int signum)
 { //signal handler
@@ -16,18 +18,19 @@ void alarmSignalHandler(int signum)
 }
 
 void exitSignalHandler(int sigint) {
-  printf("Your Program took %d seconds \n",seconds);
+  printf("Your Program excecuted %d alarms in %ld seconds \n",alarms, (time(NULL)-seconds));
   exit(1);
 }
 
 int main(int argc, char * argv[]) {
+  seconds = time(NULL);
   printf("Your program is running...\n");
   signal(SIGALRM, alarmSignalHandler);
   signal(SIGINT, exitSignalHandler);
   while (1) {
      alarm(1);
      while(flag == 0){}; 
-     seconds += 1;
+     alarms += 1;
      flag = 0;
   }
   return 0;
